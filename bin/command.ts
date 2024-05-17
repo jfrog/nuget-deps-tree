4016caf7a61c3546ab8bf3c55a1b76bb0c54c60d
Andrew Yang <evil_eve_live@outlook.com>
2024-05-18 00:30:31 +0800

    format code.

--- bin/command.ts
+++ bin/command.ts
@@ -3,32 +3,30 @@ import { NugetDepsTree, Project, DependencyTree } from '../src';
 const { program } = require('commander');
 
 program
-  .name('nuget-deps-tree')
-  .description("Nuget Dependency Tree Generator.")
-  .argument("<sln>", "solution file path")
-  .option('--text', "print it in plain text format")
+    .name('nuget-deps-tree')
+    .description('Nuget Dependency Tree Generator.')
+    .argument('<sln>', 'solution file name')
+    .option('--text', 'print it in plain text format');
 
 program.parse();
 
 const options = program.opts();
 
-const depsTree: any = NugetDepsTree.generate(program.args[0])
+const depsTree: any = NugetDepsTree.generate(program.args[0]);
 
 function print(tree: DependencyTree, level: number = 0) {
-    const prefix = "  ".repeat(level)
-    console.info(`${prefix}${tree.id}@${tree.version}`)
-    tree.dependencies.forEach((dep: any) => print(dep, level + 1))
+    const prefix = '  '.repeat(level);
+    console.info(`${prefix}${tree.id}@${tree.version}`);
+    tree.dependencies.forEach((dep: any) => print(dep, level + 1));
 }
 
 function printProject(project: Project) {
-    console.info(project.name)
-    project.dependencies.forEach((dep: any) => print(dep, 1))
+    console.info(project.name);
+    project.dependencies.forEach((dep: any) => print(dep, 1));
 }
 
-if(options.text)
-{
-    depsTree.projects.forEach((project: Project) => printProject(project))
-} else
-{
-    console.info(JSON.stringify(depsTree, null, 2))
+if (options.text) {
+    depsTree.projects.forEach((project: Project) => printProject(project));
+} else {
+    console.info(JSON.stringify(depsTree, null, 2));
 }
