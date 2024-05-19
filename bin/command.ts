6e8c60ce065f0a3657d2d99734904a0379de9bcd
Andrew Yang <evil_eve_live@outlook.com>
2024-05-19 15:31:19 +0800

    Print the dependencies in plaintext tree (#35)

--- bin/command.ts
+++ bin/command.ts
@@ -1,15 +1,32 @@
 #! /usr/bin/env node
-import { NugetDepsTree } from '../src';
+import { NugetDepsTree, Project, DependencyTree } from '../src';
+const { program } = require('commander');
 
-const args = process.argv.slice(2);
-if (args.length !== 1) {
-    throw new Error("Wrong number of arguments. Command accepts only one argument, which is a path to a solution file.")
+program
+    .name('nuget-deps-tree')
+    .description('Nuget Dependency Tree Generator.')
+    .argument('<sln>', 'solution file path')
+    .option('--text', 'print it in plain text format');
+
+program.parse();
+
+const options = program.opts();
+
+const depsTree: any = NugetDepsTree.generate(program.args[0]);
+
+function print(tree: DependencyTree, level: number = 0) {
+    const prefix = '  '.repeat(level);
+    console.info(`${prefix}${tree.id}@${tree.version}`);
+    tree.dependencies.forEach((dep: any) => print(dep, level + 1));
 }
 
-if (["-h", "-help", "help"].includes(args[0])) {
-    console.info("nuget-deps-tree - Nuget Dependency Tree Generator.\nUsage: nuget-deps-tree [path to sln file]")
-    process.exit(0)
+function printProject(project: Project) {
+    console.info(project.name);
+    project.dependencies.forEach((dep: any) => print(dep, 1));
 }
 
-const depsTree: any = NugetDepsTree.generate(args[0])
-console.info(JSON.stringify(depsTree, null, 2))
+if (options.text) {
+    depsTree.projects.forEach((project: Project) => printProject(project));
+} else {
+    console.info(JSON.stringify(depsTree, null, 2));
+}
