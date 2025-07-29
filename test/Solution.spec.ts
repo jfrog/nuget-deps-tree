283d265201bb973bdb5abb83effd1976976beff4
attiasas <assafattias93@gmail.com>
2025-07-29 13:22:07 +0300

    add tests

--- test/Solution.spec.ts
+++ test/Solution.spec.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { ProjectBuilder } from '../src/Structure/Project';
 import { Solution } from '../src/Structure/Solution';
+import * as pathUtils from 'path';
 
 describe('Solution Tests', () => {
     const packageReferences: string = path.join(__dirname, 'resources', 'packagereferences');
@@ -77,5 +78,91 @@ describe('Solution Tests', () => {
                     fail('unexpected project ' + project.name);
             }
         }
+    });
+    
+    describe('loadProject dependencies source matching', () => {
+        function createDummyProjectBuilder(
+            name: string,
+            rootPath: string,
+            dependenciesSource: string
+        ): any {
+            return {
+                name,
+                rootPath,
+                dependenciesSource,
+                extractor: true
+            };
+        }
+
+        test('matches dependencies source in project root', () => {
+            const projectName: string = 'myproject';
+            const rootPath: string = '/repo/projects/myproject';
+            const csprojPath: string = pathUtils.join(rootPath, 'myproject.csproj');
+            const depSource: string = pathUtils.join(rootPath, 'project.assets.json');
+            const solution: Solution = new Solution('/repo/projects/solution.sln');
+            solution.dependenciesSources = [depSource];
+            // Patch ProjectBuilder.load to return dummy
+            (ProjectBuilder as any).load = jest.fn(() => createDummyProjectBuilder(projectName, rootPath, depSource));
+            solution.loadProject(projectName, csprojPath);
+            expect(solution.projects).toHaveLength(1);
+            expect(solution.projects[0].dependenciesSource).toBe(depSource);
+        });
+
+        test('matches dependencies source under project directory', () => {
+            const projectName: string = 'myproject';
+            const rootPath: string = '/repo/projects/myproject';
+            const csprojPath: string = pathUtils.join(rootPath, 'myproject.csproj');
+            const depSource: string = pathUtils.join(rootPath, 'subdir', 'project.assets.json');
+            const solution: Solution = new Solution('/repo/projects/solution.sln');
+            solution.dependenciesSources = [depSource];
+            (ProjectBuilder as any).load = jest.fn(() => createDummyProjectBuilder(projectName, rootPath, depSource));
+            solution.loadProject(projectName, csprojPath);
+            expect(solution.projects).toHaveLength(1);
+            expect(solution.projects[0].dependenciesSource).toBe(depSource);
+        });
+
+        test('matches dependencies source ending with project name', () => {
+            const projectName: string = 'myproject';
+            const rootPath: string = '/repo/projects/myproject';
+            const csprojPath: string = pathUtils.join(rootPath, 'myproject.csproj');
+            const depSource: string = '/repo/projects/otherdir/myproject';
+            const solution: Solution = new Solution('/repo/projects/solution.sln');
+            solution.dependenciesSources = [depSource];
+            (ProjectBuilder as any).load = jest.fn(() => createDummyProjectBuilder(projectName, rootPath, depSource));
+            solution.loadProject(projectName, csprojPath);
+            expect(solution.projects).toHaveLength(1);
+            expect(solution.projects[0].dependenciesSource).toBe(depSource);
+        });
+
+        test('multiple sources, only correct one matched', () => {
+            const projectName: string = 'myproject';
+            const rootPath: string = '/repo/projects/myproject';
+            const csprojPath: string = pathUtils.join(rootPath, 'myproject.csproj');
+            const depSource1: string = '/repo/projects/otherdir/otherproject';
+            const depSource2: string = pathUtils.join(rootPath, 'project.assets.json');
+            const depSource3: string = '/repo/projects/otherdir/myproject';
+            const solution: Solution = new Solution('/repo/projects/solution.sln');
+            solution.dependenciesSources = [depSource1, depSource2, depSource3];
+            (ProjectBuilder as any).load = jest.fn(() => createDummyProjectBuilder(projectName, rootPath, depSource2));
+            solution.loadProject(projectName, csprojPath);
+            expect(solution.projects).toHaveLength(1);
+            expect(solution.projects[0].dependenciesSource).toBe(depSource2);
+        });
+
+        test('no matching source, dependenciesSource is empty', () => {
+            const projectName: string = 'myproject';
+            const rootPath: string = '/repo/projects/myproject';
+            const csprojPath: string = pathUtils.join(rootPath, 'myproject.csproj');
+            const depSource: string = '/repo/projects/otherdir/otherproject';
+            const solution: Solution = new Solution('/repo/projects/solution.sln');
+            solution.dependenciesSources = [depSource];
+            (ProjectBuilder as any).load = jest.fn((name: string, root: string, source: string) => {
+                expect(source).toBe('');
+                return createDummyProjectBuilder(name, root, source);
+            });
+            solution.loadProject(projectName, csprojPath);
+            expect(solution.projects).toHaveLength(1);
+            expect(solution.projects[0].dependenciesSource).toBe('');
+        });
     });
 });
